80dea6526c9056e9190321e8a4b82bff9cfb7174
mohammadmunnamia0 <mohammadmunnamia0@gmail.com>
2024-12-28 00:37:24 +0600

    regester complete,sign in comp,email validation

--- src/Components/RegComp/RegComp.jsx
+++ src/Components/RegComp/RegComp.jsx
@@ -1,22 +1,18 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, sendEmailVerification } from "firebase/auth";
 import auth from "../../Firebase/firebase.config";
 import { useState } from "react";
+import { FaEye,FaEyeSlash } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
 
 const RegComp = () => {
-
-
-
-
-
   //2.1 Show register error {when user is already added cant add the same user again}
   const [registerError, setRegisterError] = useState("");
 
   //3.1 Show Successfully added user
   const [SuccessfullyAddedUser, setSuccessfullyAddedUser] = useState();
 
-
-
-
+  //showPassword 
+  const [ShowPassword,serShowPassword]=useState(false);
 
 
   //get the values from the each field of the form
@@ -25,27 +21,33 @@ const RegComp = () => {
     const name = e.target.name.value;
     const email = e.target.email.value;
     const password = e.target.password.value;
-    console.log(name, email, password);
+    const AcceptTerm = e.target.terms.checked;
+    console.log(name, email, password,AcceptTerm);
     console.log("submitted");
 
 
-      //2.4 Reset/Clear the Error Message Before the new Registration
-      setRegisterError("");
-
-      //3.4 Reset/Clear the Successfully Added User Message Before the new Registration
-      setSuccessfullyAddedUser("");
+    //2.4 Reset/Clear the Error Message Before the new Registration
+    setRegisterError("");
 
 
+    //3.4 Reset/Clear the Successfully Added User Message Before the new Registration
+    setSuccessfullyAddedUser("");
 
 
     //4. checking the password error if password is less then 6 then show an error message
     if (password.length < 6) {
       setRegisterError("Password must be at least 6 characters");
       return; // ai khane return korar kaorn jodi pass vul hoy less then 6 tahole r code samne excute hobe na
+    } 
+    else if (!/[A-Z]/.test(password)) {
+      setRegisterError("Password must Contain a Capital characters");
+      return;
     }
-      
+    else if (!AcceptTerm) {
+      setRegisterError("Accept Our Terms and Conditions");
+      return;
+    }
 
-  
 
     //1. firebase authentication create user
 
@@ -54,19 +56,18 @@ const RegComp = () => {
         const RegUser = result.user;
         console.log(RegUser);
         setSuccessfullyAddedUser(RegUser); //3.2
+
+        //firebase send verification mail to verify the user email
+        sendEmailVerification(result.user)
+        .then(()=>{
+          alert("Check Your Mail to Verify the Email")
+        })
       })
       .catch((error) => {
         console.error(error);
         setRegisterError(error.message); //2.2
       });
   };
-
-
-
-
-
-
-
 
   return (
     <div>
@@ -78,7 +79,7 @@ const RegComp = () => {
             </p>
 
             <form onSubmit={HandleRegister}>
-              <div className="w-full mt-4">
+              {/* <div className="w-full mt-4">
                 <input
                   className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
                   name="name"
@@ -86,7 +87,7 @@ const RegComp = () => {
                   aria-label="Email Address"
                   required
                 />
-              </div>
+              </div> */}
               <div className="w-full mt-4">
                 <input
                   className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
@@ -97,14 +98,25 @@ const RegComp = () => {
                 />
               </div>
 
-              <div className="w-full mt-4">
+              <div className="w-full mt-4 flex relative">
                 <input
                   className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
                   name="password"
+                  type={ShowPassword ? "text" : "password"}
                   placeholder="Password"
                   aria-label="Password"
                   required
                 />
+                <span onClick={()=> serShowPassword(!ShowPassword)} className="text-2xl absolute right-4 top-4">
+                {
+                  ShowPassword ?  <FaEye />  :    <FaEyeSlash />
+                }
+                </span>
+              </div>
+
+              <div className="mt-4">
+                <input type="checkbox" name="terms" id="terms"/>
+                <label className="ml-2 text-" htmlFor="terms">Accept our terms and Conditions</label>
               </div>
 
               <div className="!mt-12">
@@ -117,23 +129,28 @@ const RegComp = () => {
               </div>
             </form>
 
-
-
-
-
-
-
             {/*2.3. Showing the error */}
             {registerError && <p className="text-red-600">{registerError}</p>}
 
             {/*3.3 Showing the Successfully Added User */}
 
-            {SuccessfullyAddedUser && (<p className="text-green-400">User Added Successfully</p>
-
-
-
+            {SuccessfullyAddedUser && (
+              <p className="text-green-400 font-bold text-xl">Account Created Successfully</p>
             )}
           </div>
+
+          <div className="flex items-center justify-center py-4 text-center bg-gray-50 dark:bg-gray-700">
+          <span className="text-sm text-gray-600 dark:text-gray-200">
+            Already Haven an Account{" "}
+          </span>
+
+          <NavLink to="/signin"
+            href="#"
+            className="mx-2 text-sm font-bold text-blue-500 dark:text-blue-400 hover:underline"
+          >
+            Login
+          </NavLink>
+        </div>
 
           <button
             type="button"
--- src/Components/SignIn.jsx/SignIn.jsx
+++ src/Components/SignIn.jsx/SignIn.jsx
@@ -1,16 +1,71 @@
+import { sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
 import { NavLink } from "react-router-dom";
+import auth from "../../Firebase/firebase.config";
+import { useRef, useState } from "react";
 
 const SignIn = () => {
+  const [registerError, setRegisterError] = useState("");
+
+  const [SuccessfullyAddedUser, setSuccessfullyAddedUser] = useState();
+
+  const emailRef = useRef(); //jehetu forgot password is not a part of form tai forgot pasword a press korle amar to form a dewa email ke pabo na , tai useref korlam so that amra user ar mail ta form theke reference hisab a niye oi mail ar moddhe akta forgot mail ar message dite pari
+
+  const HandleSignin = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
+
+    //clear before new log
+    setRegisterError("");
+
+    //validation signInWithEmailAndPassword
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result.user);
+        if(result.user.emailVerified)
+          {
+          console.log("Email is verified");
+        }
+        else
+         {
+          alert("Email is not verified");
+        }
+        setSuccessfullyAddedUser(result.user);
+      })
+      
+      .catch((error) => {
+        console.log(error.message);
+        setRegisterError(error.message);
+      });
+  };
 
 
- const HandleSignin = e => {
-  e.preventDefault();
-  const email = e.target.email.value;
-  const pass = e.target.password.value;
-  console.log(email,pass);
- }
 
+  //forgot password , useref
 
+  const handleForgotPassword = () => {
+    const email = emailRef.current.value;
+    if (!email) {
+      alert("Please enter your email", emailRef.current.value);
+      return;
+    } 
+    
+    else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+      alert("Enter a Valid Email");
+      return
+    }
+
+    //firebase Forgot password
+    sendPasswordResetEmail(auth,email)
+    .then(()=>{
+      alert('Check Your Email')
+    })
+    .catch(error =>{
+      console.log(error)
+    })
+
+  };
 
   return (
     <div>
@@ -25,6 +80,7 @@ const SignIn = () => {
               <input
                 className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
                 name="email"
+                ref={emailRef}
                 placeholder="Email Address"
                 aria-label="Email Address"
               />
@@ -41,16 +97,24 @@ const SignIn = () => {
 
             <div className="flex items-center justify-between mt-4">
               <a
+                onClick={handleForgotPassword}
                 href="#"
                 className="text-sm text-gray-600 dark:text-gray-200 hover:text-gray-500"
               >
                 Forget Password?
               </a>
 
-              <button  className="px-6 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+              <button className="px-6 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
                 Sign In
               </button>
             </div>
+            {registerError && <p className="text-red-600">{registerError}</p>}
+
+            {SuccessfullyAddedUser && (
+              <p className="text-green-400 font-bold text-xl">
+                Login Successful
+              </p>
+            )}
           </form>
         </div>
 
@@ -59,7 +123,8 @@ const SignIn = () => {
             Dont have an account?{" "}
           </span>
 
-          <NavLink to="/register"
+          <NavLink
+            to="/register"
             href="#"
             className="mx-2 text-sm font-bold text-blue-500 dark:text-blue-400 hover:underline"
           >
